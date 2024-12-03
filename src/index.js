10ce3847db66c34a682a55065de2e30bb483f008
shantpr05 <shanthi.parvatham@hyperisland.se>
2024-12-03 13:26:24 +0100

    products display

--- src/index.js
+++ src/index.js
@@ -1,13 +1,15 @@
 import "./style.css";
+import "./products-style.css"
+import "./categories-style.css"
 import { getData } from "./scripts/api/fetchFakeProducts";
 import { productPage } from "./scripts/pages/productpage";
 import { categoriePage } from "./scripts/pages/categoriePage";
  
     // Product page
-    productPage();
+    //productPage();
 
     // Categorie page
-    // categoriePage();
+    categoriePage();
 
     // Cart 
 
