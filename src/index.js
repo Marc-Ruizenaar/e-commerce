028e313ce036a256d7780ef7395f24cbf76aae8c
Gökşin <79875481+goksinb@users.noreply.github.com>
2024-12-03 15:29:16 +0100

    add homepage (#6)

--- src/index.js
+++ src/index.js
@@ -1,16 +1,20 @@
 import "./style.css";
-import "./products-style.css"
-import "./categories-style.css"
-import { getData } from "./scripts/api/fetchFakeProducts";
-import { productPage } from "./scripts/pages/productpage";
-import { categoriePage } from "./scripts/pages/categoriePage";
- 
-    // Product page
-    //productPage();
+import "./products-style.css";
+import "./categories-style.css";
+import {getData} from "./scripts/api/fetchFakeProducts";
+import {productPage} from "./scripts/pages/productpage";
+import {categoriePage} from "./scripts/pages/categoriePage";
+import {renderHomepage} from "./scripts/pages/homepage";
 
-    // Categorie page
-    categoriePage();
+// Product page
+//productPage();
 
-    // Cart 
+// Categorie page
+categoriePage();
 
-    // Checkout
+// Cart
+
+// Checkout
+
+// Homepage
+renderHomepage();
