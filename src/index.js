a62dc0eabd9d4c9b13135038cd186e2773878744
Adele Zilinskaite <zilinskaite.adele@gmail.com>
2024-12-04 10:40:25 +0100

    Cart: close button working

--- src/index.js
+++ src/index.js
@@ -4,13 +4,16 @@ import "./styles/style.css";
 import "./styles/products-style.css";
 import "./styles/categories-style.css";
 import "./styles/homepage-style.css";
+import "./styles/cart.css";
 import { getData } from "./scripts/api/fetchFakeProducts";
 import { productPage } from "./scripts/pages/productpage";
 import { categoriePage } from "./scripts/pages/categoriePage";
 import { renderHomepage } from "./scripts/pages/homepage";
 import { header } from "./scripts/header/headercomponant";
+import { cartDisplay } from "./scripts/cart/cart";
 
 header();
+cartDisplay();
 
 // Product page
 //productPage();
