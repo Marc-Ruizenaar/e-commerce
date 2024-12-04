a0056366198dd8a36260e9314f7cec1a4d916039
Yali Zhang <90899676+YaliZhang1@users.noreply.github.com>
2024-12-04 10:37:18 +0100

    fix the close button, make it works (#7)

--- src/scripts/header/headercomponant.js
+++ src/scripts/header/headercomponant.js
@@ -5,20 +5,21 @@ import imageF from "../../assets/img/man-clothes.jpg";
 import imageFi from "../../assets/img/jewelry.jpg";
 import imageSi from "../../assets/img/electronic.jpg";
 
-
 export function header() {
+  const header = document.getElementById("header");
 
-    const header = document.getElementById("header");
-
-    header.innerHTML = `
-<header>
-<div class="topButtons">
-  <button id="close-btn" class="span-color">CLOSE</button>
-  <img id="logo-big" class="span-color" src="${image}"></img>
-  <button id="cart-btn" class="span-color">CART</button>
+  header.innerHTML = `
+<header >
+<div id="menu-header">
+  <div id="topButtons">
+    <button id="menu-btn" class="span-color">MENU</button>
+    <button id="close-btn" class="span-color">CLOSE</button>
+    <img id="logo-big" class="span-color" src="${image}"></img>
+    <button id="cart-btn" class="span-color">CART</button>
+  </div>
 </div>
-<div class="topMenu">
-  <ul>
+<div id="topMenu">
+  <ul id="topMenuUl">
     <li>
       <a id="shop-all-btn" class="shopBtn" data-bg="${imageTwo}" href="#">
         <div class="span-color">
@@ -49,49 +50,63 @@ export function header() {
   </ul>
 </div>
 </header>`;
+  const buttonContainer = document.querySelector("body");
+  document.getElementById("close-btn").onclick = function () {
+    document.getElementById("menu-btn").style.display = "block";
+    document.getElementById("close-btn").style.display = "none";
+    document.getElementById("menu-header").style.backgroundColor = "#f3f1e0";
+    document.getElementById("topMenu").style.display = "none";
 
+    buttonContainer.style.backgroundImage = "none";
+  };
+  document.getElementById("menu-btn").onclick = function () {
+    document.getElementById("close-btn").style.display = "block";
+    document.getElementById("menu-btn").style.display = "none";
+    document.getElementById("topMenu").style.display = "flex";
 
-    document.getElementById("close-btn").onclick = function () {
-        window.location.href = "https://www.example.com";
-    };
-    document.getElementById("cart-btn").onclick = function () {
-        window.location.href = "https://www.example.com";
-    };
+    document.getElementById("topMenuUl").style.display = "flex";
+    document.getElementById("menu-header").style.backgroundColor =
+      "transparent";
+    document.getElementById("topMenu").style.display = "flex";
+  };
+  document.getElementById("cart-btn").onclick = function () {
+    window.location.href = "https://www.example.com";
+  };
 
+  const shopBtns = document.querySelectorAll(".shopBtn");
+
+  const buttonSpanColors = document.querySelectorAll(".span-color");
+
+  document.addEventListener("DOMContentLoaded", () => {
     const shopBtns = document.querySelectorAll(".shopBtn");
-    const buttonContainer = document.querySelector("body");
-    const buttonSpanColors = document.querySelectorAll(".span-color");
-
-    document.addEventListener("DOMContentLoaded", () => {
-        const shopBtns = document.querySelectorAll(".shopBtn");
-        shopBtns.forEach((button) => {
-            const img = new Image();
-            img.src = button.getAttribute("data-bg");
-        });
+    shopBtns.forEach((button) => {
+      const img = new Image();
+      img.src = button.getAttribute("data-bg");
     });
 
     shopBtns.forEach((button) => {
-        button.addEventListener("mouseenter", () => {
-            const bgImage = button.getAttribute("data-bg");
-            buttonContainer.style.backgroundImage = `url('${bgImage}')`;
-        });
+      button.addEventListener("mouseenter", () => {
+        const bgImage = button.getAttribute("data-bg");
+        buttonContainer.style.backgroundImage = `url('${bgImage}')`;
+        document.getElementById("topButtons").style.backgroundColor =
+          "transparent";
+      });
     });
+  });
 
-    buttonSpanColors.forEach((btn) => {
-        btn.addEventListener("mouseenter", () => {
-            btn.classList.add("transparent");
-            btn.classList.add("active");
+  buttonSpanColors.forEach((btn) => {
+    btn.addEventListener("mouseenter", () => {
+      btn.classList.add("transparent");
+      btn.classList.add("active");
 
-            buttonSpanColors.forEach((btnOther) => {
-                if (btnOther !== btn) {
-                    btnOther.classList.add("transparent");
-                }
-            });
-        });
-        btn.addEventListener("mouseleave", () => {
-            btn.classList.remove("active");
-        });
+      buttonSpanColors.forEach((btnOther) => {
+        if (btnOther !== btn) {
+          btnOther.classList.add("transparent");
+        }
+      });
     });
-
-
+    btn.addEventListener("mouseleave", () => {
+      btn.classList.remove("active");
+    });
+  });
 }
